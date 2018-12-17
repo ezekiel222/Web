b32e046c8dc0f7ea9915e4fab91d4a18675b1c70
root <root@pop-os.localdomain>
2018-12-17 00:24:35 -0300

    navbar

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,25 @@
+class RegistrationsController < Devise::RegistrationsController
+  private
+  def sign_up_params
+    params.require(:user).permit(
+      :email,
+      :password,
+      :password_confirmation,
+      :username,
+      :fistname,
+      :lastname
+      )
+  end
+
+  def account_update_params
+    params.require(:user).permit(
+      :email,
+      :password,
+      :password_confirmation,
+      :current_password,
+      :username,
+      :fistname,
+      :lastname
+      )
+  end
+end
